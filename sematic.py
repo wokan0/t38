e3daa17a69906fd9b9e350425253fc6decb23a24
Wojciech Kania <wokan00@gmail.com>
2022-12-09 19:09:22 +0000

    Second extract

--- sematic.py
+++ sematic.py
@@ -1,8 +1,9 @@
 # importing spacy and loading the model
 import spacy
+
 nlp = spacy.load('en_core_web_md')
 
-# words to compare
+# Words to compare from first code extract
 word1 = nlp("cat")
 word2 = nlp("monkey")
 word3 = nlp("banana")
@@ -10,3 +11,10 @@ word3 = nlp("banana")
 print(word1.similarity(word2))
 print(word3.similarity(word2))
 print(word3.similarity(word1))
+
+# Second code extract
+tokens = nlp('cat apple monkey banana')
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
