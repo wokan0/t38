c03b33c8cca2b7644af1cafd88243c799fe849a4
Wojciech Kania <wokan00@gmail.com>
2022-12-09 19:57:39 +0000

    Third extract

--- sematic.py
+++ sematic.py
@@ -18,3 +18,18 @@ tokens = nlp('cat apple monkey banana')
 for token1 in tokens:
     for token2 in tokens:
         print(token1.text, token2.text, token1.similarity(token2))
+
+# Third extract
+sentence_to_compare = "Why is my cat on the car"
+
+sentences = ["where did my dog go",
+             "Hello, there is my car",
+             "I\'ve lost my car in my car",
+             "I\'d like my boat back",
+             "I will name my dog Diana"]
+
+model_sentence = nlp(sentence_to_compare)
+
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
